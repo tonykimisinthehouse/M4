73392b2370e9705f41878abea7a3231b5816135b
Tony Kim <tonykiminkoreaaa@gmail.com>
2023-11-13 16:06:36 -0500

    Added CORS policy to enable frontend applications like React to fetch data from the backend

--- backend.py
+++ backend.py
@@ -1,11 +1,25 @@
 import time
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import psycopg2
 from configs import pg_connection_dict
 from sql_generators import *
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/get_time_series")
