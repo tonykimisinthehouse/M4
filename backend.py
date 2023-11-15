bae886e755435f9447ce0f8c4ddf8570865a3270
Tony Kim <tonykiminkoreaaa@gmail.com>
2023-11-15 15:21:57 -0500

    Added API for calculating visualization quality; Added query execution time for time series data queries

--- backend.py
+++ backend.py
@@ -1,11 +1,16 @@
 import time
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 import psycopg2
 from configs import pg_connection_dict
 from sql_generators import *
+
+from skimage.metrics import structural_similarity
+import cv2
+import numpy as np
+
 app = FastAPI()
 
 origins = [
@@ -24,7 +29,6 @@ app.add_middleware(
 
 @app.get("/get_time_series")
 def time_series_query_for_visualization(from_time: int, to_time: int, chart_width: int, chart_height: int):
-    start_time = time.perf_counter()
     # Connect to your postgres DB
     conn = psycopg2.connect(**pg_connection_dict)
 
@@ -34,6 +38,7 @@ def time_series_query_for_visualization(from_time: int, to_time: int, chart_widt
     time_column = 'epoch'
     value_column = 'price'
 
+    start_time = time.perf_counter()
     sql_query = f"""
     WITH Q AS ({select_query_raw_time_series('jrufjdm3qog6zerz_w_epoch', time_column, value_column, from_time, to_time)}),
     QC AS (SELECT count(*) as c from Q)
@@ -42,21 +47,18 @@ def time_series_query_for_visualization(from_time: int, to_time: int, chart_widt
     {sql_m4_aggregation("Q", time_column, value_column, from_time, to_time, chart_width)}
     ) AS QD wHERE (SELECT c FROM QC) > {4 * chart_width} ORDER BY {time_column}
     """
-    print(sql_query)
     cur.execute(sql_query)
 
     # Retrieve query results
     records = cur.fetchall()
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
-    print("Elapsed time: ", elapsed_time)
 
-    return records
+    return {"records": records, "query_execution_time": elapsed_time}
 
 
 @app.get("/get_raw_time_series")
 def time_series_query_for_visualization(from_time: int, to_time: int):
-    start_time = time.perf_counter()
     # Connect to your postgres DB
     conn = psycopg2.connect(**pg_connection_dict)
 
@@ -66,6 +68,7 @@ def time_series_query_for_visualization(from_time: int, to_time: int):
     time_column = 'epoch'
     value_column = 'price'
 
+    start_time = time.perf_counter()
     cur.execute(f"""
     {select_query_raw_time_series('jrufjdm3qog6zerz_w_epoch', time_column, value_column, from_time, to_time)}
     """)
@@ -74,9 +77,33 @@ def time_series_query_for_visualization(from_time: int, to_time: int):
     records = cur.fetchall()
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
-    print("Elapsed time: ", elapsed_time)
 
-    return records
-
+    return {"records": records, "query_execution_time": elapsed_time}
 
 
+@app.post("/compute_dssim")
+async def compute_dssim(image_1: UploadFile = File(...), image_2: UploadFile = File(...)):
+    print(image_1.content_type)
+    image_1_content = await image_1.read()
+    image_2_content = await image_2.read()
+    image_1_np_arr = np.fromstring(image_1_content, np.uint8)
+    print(image_1_np_arr.shape)
+    image_2_np_arr = np.fromstring(image_2_content, np.uint8)
+    print(image_2_np_arr.shape)
+    np.savetxt('test1.txt', image_1_np_arr, fmt='%d')
+    np.savetxt('test2.txt', image_2_np_arr, fmt='%d')
+
+    image_1_cv2 = cv2.imdecode(image_1_np_arr, cv2.IMREAD_COLOR)
+    image_2_cv2 = cv2.imdecode(image_2_np_arr, cv2.IMREAD_COLOR)
+    print(image_1_cv2.shape)
+    print(image_2_cv2.shape)
+
+    # Convert images to grayscale
+    image_1_gray = cv2.cvtColor(image_1_cv2, cv2.COLOR_BGR2GRAY)
+    image_2_gray = cv2.cvtColor(image_2_cv2, cv2.COLOR_BGR2GRAY)
+    (ssim, diff) = structural_similarity(image_1_gray, image_2_gray, full=True)
+    dssim = (1 - ssim) / 2
+    return {
+        "dssim": dssim,
+        "ssim": ssim
+    }
